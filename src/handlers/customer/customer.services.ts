444be5a9d6da711ae8ad7f4e48570c496708b242
Kasjful Kurniawan <kasful.bpp@gmail.com>
2022-10-19 20:53:13 +0800

    fix: change query of customer

--- src/handlers/customer/customer.services.ts
+++ src/handlers/customer/customer.services.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
+import VoucherServices from '../voucher/voucher.services';
 
-export default class VoucherServices {
+export default class CustomerServices {
   private prisma = new PrismaClient()
+
+  private voucher= new VoucherServices()
 
   getAll = async (req: Request, res: Response): Promise<void> => {
     const results = await this.prisma.customers.findMany();
@@ -10,11 +13,29 @@ export default class VoucherServices {
   };
 
   getId = async (req: Request, res: Response): Promise<void> => {
-    const { id } = req.params;
-    const result = await this.prisma.customers.findFirst({
-      where: { id: Number(id) },
-    });
-    res.send(result);
+    try {
+      const { id } = req.params;
+      const result = await this.prisma.customers.findFirstOrThrow({
+        where: { id: Number(id) },
+        select: {
+          id: true,
+          first_name: true,
+          last_name: true,
+          contact_number: true,
+          email: true,
+          PurchaseTransaction: true,
+        },
+      });
+      const checkEligible = await this.voucher.checkEligible(Number(id));
+      console.log(result);
+      res.send({ ...result, is_eligible: checkEligible.length > 0 });
+    } catch (error) {
+      if (error.name === 'NotFoundError') {
+        res.status(404).send(error);
+        return;
+      }
+      res.status(500).send(error);
+    }
   };
 
   validateCustomer = async (req: Request, res: Response): Promise<void> => {
